bfa3210d98b0fee04de30a22ca666ded25b7a8d7
guilhermefgl <Leonardo.souza@Montreal.com.br>
2018-03-06 17:21:59 -0300

    bottom navigation layout

--- app/src/main/java/com/guilhermefgl/peliculas/widgets/ScrollingFooterBehavior.java
+++ app/src/main/java/com/guilhermefgl/peliculas/widgets/ScrollingFooterBehavior.java
@@ -1,47 +1,60 @@
 package com.guilhermefgl.peliculas.widgets;
 
 import android.content.Context;
-import android.support.design.widget.AppBarLayout;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
-/* https://medium.com/@nullthemall/bottom-navigation-behavior-388b9b206667
+/* https://stackoverflow.com/a/44778453
  */
-public class ScrollingFooterBehavior extends AppBarLayout.ScrollingViewBehavior {
+public class ScrollingFooterBehavior  extends CoordinatorLayout.Behavior<BottomNavigationView> {
 
-    private AppBarLayout appBarLayout;
-
-    public ScrollingFooterBehavior() {
-        super();
-    }
+    private int height;
 
     public ScrollingFooterBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
-        if (appBarLayout == null) {
-            appBarLayout = (AppBarLayout) dependency;
-        }
-
-        final boolean result = super.onDependentViewChanged(parent, child, dependency);
-        final int bottomPadding = calculateBottomPadding(appBarLayout);
-        final boolean paddingChanged = bottomPadding != child.getPaddingBottom();
-        if (paddingChanged) {
-            child.setPadding(
-                    child.getPaddingLeft(),
-                    child.getPaddingTop(),
-                    child.getPaddingRight(),
-                    bottomPadding);
-            child.requestLayout();
-        }
-
-        return paddingChanged || result;
+    public boolean onLayoutChild(CoordinatorLayout parent, BottomNavigationView child, int layoutDirection) {
+        height = child.getHeight();
+        return super.onLayoutChild(parent, child, layoutDirection);
     }
 
-    private int calculateBottomPadding(AppBarLayout dependency) {
-        return dependency.getTotalScrollRange() + dependency.getTop();
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                                       @NonNull BottomNavigationView child,
+                                       @NonNull View directTargetChild,
+                                       @NonNull View target,
+                                       int axes, int type)
+    {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                               @NonNull BottomNavigationView child,
+                               @NonNull View target, int dxConsumed, int dyConsumed,
+                               int dxUnconsumed, int dyUnconsumed,
+                               @ViewCompat.NestedScrollType int type)
+    {
+        if (dyConsumed > 0) {
+            slideDown(child);
+        } else if (dyConsumed < 0) {
+            slideUp(child);
+        }
+    }
+
+    private void slideUp(BottomNavigationView child) {
+        child.clearAnimation();
+        child.animate().translationY(0).setDuration(200);
+    }
+
+    private void slideDown(BottomNavigationView child) {
+        child.clearAnimation();
+        child.animate().translationY(height).setDuration(200);
     }
 }
